b543d199028dfd28e3d3f6058e6a1f811e20e1bd
lovedrones <JuanTron@juans-mbp.attlocal.net>
2020-01-03 08:05:58 -0600

    full crud added

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
 
 class Finch(models.Model):
@@ -9,4 +9,11 @@ class Finch(models.Model):
     age = models.IntegerField()
 
     def __str__(self):
-     return f'{self.name} ({self.id})'
+     return self.name
+
+    def get_absolute_url(self):
+       return reverse("detail", kwargs={"finch_id": self.id})
+
+class Toy(models.Model):
+      name = models.CharField(max_length=50)
+      color = models.CharField(max_length=20)
